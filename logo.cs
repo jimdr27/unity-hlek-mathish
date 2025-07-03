dd3585f27e2d903b45587ba9f0f4099c3c81ecec
jimdr27 <jimdrymos27@gmail.com>
2025-07-03 22:46:43 +0300

    Add files via upload
    this is a project copied from flygamesstudio from youtube the idea belongs to him

---
+++ logo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class logo : MonoBehaviour {
+
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("menu");
+    }
+
+}
